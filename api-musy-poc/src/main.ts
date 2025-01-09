d5f39c60ed324547601576cebf44e5bc3f64d9e4
Lucas Huerta <huertalucas13@gmail.com>
2025-01-09 11:41:14 +0100

    🐛 fix cors bug

--- api-musy-poc/src/main.ts
+++ api-musy-poc/src/main.ts
@@ -4,12 +4,27 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from "@nestjs/platform-fastify";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
   );
+
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: configService.get<string>("CORS_ORIGIN", "*").split(", "),
+    credentials: true,
+    exposedHeaders: configService
+      .get<string>("CORS_EXPOSED_HEADERS", "")
+      .split(", "),
+    methods: configService.get<string>("CORS_ALLOWED_METHODS", "").split(", "),
+    allowedHeaders: configService
+      .get<string>("CORS_ALLOWED_HEADERS", "")
+      .split(", "),
+  });
   await app.listen(process.env.PORT || 1337);
 }
 bootstrap();
