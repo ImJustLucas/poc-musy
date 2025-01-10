d3df4890ddf8c24ee7589e3348590838436e41d3
Lucas Huerta <huertalucas13@gmail.com>
2025-01-10 12:08:04 +0100

    🐛 url back

--- client-musy-poc/src/services/fetcher/config.ts
+++ client-musy-poc/src/services/fetcher/config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
-  baseURL: process.env.API_URL ?? "http://localhost:1337",
+  // baseURL: process.env.API_URL ?? "http://localhost:1337",
+  baseURL: "https://api-musy-poc-4f55.up.railway.app",
   headers: {
     "Content-Type": "application/json",
   },
