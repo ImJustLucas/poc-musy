fb6a988af675de62859d839184c57762b1c49d85
ImJustLucas <lucas.bellier@icloud.com>
2025-01-10 10:49:40 +0100

    ✨ Enhance JoinRoom component: add Suspense for improved loading handling and log base URL

--- client-musy-poc/src/app/join-room/page.tsx
+++ client-musy-poc/src/app/join-room/page.tsx
@@ -1,5 +1,6 @@
 import RoomCard from "@/components/roomCard";
 import { Room as RoomType } from "@/shared/types/room";
+import { Suspense } from "react";
 
 type APIResponse<K> = {
   success: boolean;
@@ -8,7 +9,7 @@ type APIResponse<K> = {
 
 export default async function JoinRoom() {
   const baseURL = process.env.NEXT_PUBLIC_SOCKET_URL ?? "http://localhost:1337";
-
+  console.log(baseURL);
   const data = await fetch(`${baseURL}/room`);
   const response = (await data.json()) as APIResponse<RoomType[]>;
 
@@ -19,9 +20,11 @@ export default async function JoinRoom() {
           Liste des salles
         </h1>
 
-        {response.data.map((room, index) => (
-          <RoomCard key={index} room={room} />
-        ))}
+        <Suspense>
+          {response.data.map((room, index) => (
+            <RoomCard key={index} room={room} />
+          ))}
+        </Suspense>
       </div>
     </div>
   );
